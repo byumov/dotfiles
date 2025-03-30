76b316458b71363139bf7046d2bcad3e9f8cb6b5
Ivan Buymov <ivan@buymov.ru>
2025-03-30 13:48:12 +0300

    add gitconfig, edit ghostty

--- init.sh
+++ init.sh
@@ -3,13 +3,13 @@ mkdir -p ~/.ssh
 mkdir -p ~/.git
 mkdir -p ~/.config/ghostty
 
-
 # create links
 ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/alacritty/.alacritty.yml ~/.alacritty.yml
 ln -sf ~/dotfiles/lsd/config.yaml ~/.config/lsd/config.yaml
 ln -sf ~/dotfiles/git/.gitconfig ~/.git/
 ln -sf ~/dotfiles/ghostty/config ~/.config/ghostty/
+ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
 
 # private
 ln -sf ~/Sync/dotfiles/k8s/.kube ~/.kube
