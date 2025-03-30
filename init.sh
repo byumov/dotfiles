e9295ef808aecfa1b9a63b578ca18bbf61ba832c
Ivan Buymov <ivan.buymov@Ivans-MacBook-Pro.local>
2025-03-30 13:03:37 +0300

    updates

--- init.sh
+++ init.sh
@@ -1,7 +1,16 @@
 mkdir -p ~/.config/lsd
+mkdir -p ~/.ssh
+mkdir -p ~/.git
+mkdir -p ~/.config/ghostty
+
 
 # create links
 ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/alacritty/.alacritty.yml ~/.alacritty.yml
 ln -sf ~/dotfiles/lsd/config.yaml ~/.config/lsd/config.yaml
-ln -s ~/sync/dotfiles/aws ~/.aws
+ln -sf ~/dotfiles/git/.gitconfig ~/.git/
+ln -sf ~/dotfiles/ghostty/config ~/.config/ghostty/
+
+# private
+ln -sf ~/Sync/dotfiles/k8s/.kube ~/.kube
+ln -sf ~/Sync/dotfiles/aws ~/.aws
